495087a2ad466944a8a806f326c78d414ad2913a
GG <gerardstjosol@users.noreply.github.com>
2021-08-24 15:17:38 +0800

    Created dungeon 1 stage and movement script

---
+++ PROSDEV_TreseGame/Assets/Scripts/MoveMainChar.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMainChar : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float moveSpeed;
+    private Vector3 change;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.z = Input.GetAxisRaw("Vertical");
+        if (change != Vector3.zero)
+            MoveCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        rb.MovePosition(transform.position + change * moveSpeed * Time.deltaTime);
+    }
+}
